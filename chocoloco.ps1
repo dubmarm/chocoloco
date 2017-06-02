4e4630479809f68ab25194b37db3dab289d7613d
Dubina <dubinary@usap.gov>
2017-06-02 17:06:53 -0300

    restructured a lot of logic to fit more dynamic dependencies

--- chocoloco.ps1
+++ chocoloco.ps1
@@ -13,11 +13,13 @@ import-module C:\ProgramData\chocolatey\helpers\chocolateyInstaller.psm1
 import-module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 
 #Variables
-$pkg = "notepadplusplus.commandline"
-$pkgcache = "C:\ProgramData\chocolatey\cache"
-$pkglib = "C:\ProgramData\chocolatey\lib\$pkg"
-$pkgcache = "C:\ProgramData\chocolatey\cache\$pkg"
-$pkginstall = "$pkgnupkg\tools\chocolateyInstall.ps1"
+$mainpkg = "notepadplusplus"
+$pkg = ""
+$mainpkgcache = "C:\ProgramData\chocolatey\cache"
+$mainpkglib = "C:\ProgramData\chocolatey\lib\$mainpkg"
+$mainpkgcache = "C:\ProgramData\chocolatey\cache\$mainpkg"
+$mainpkgnupkg = "C:\ProgramData\chocolatey\nupkg\$mainpkg"
+$mainpkginstall = "$pkgnupkg\tools\chocolateyInstall.ps1"
 
 
 #downloads typically go to C:\Users\USERNAME\AppData\Local\Temp
@@ -29,52 +31,63 @@ if (($cacheLocation[1]) -ne "C:\ProgramData\chocolatey\cache") {
         Write-Host "Cache directory does not exist; creating cache directory" -foreground Yellow
         mkdir C:\ProgramData\chocolatey\cache
     }
-    choco config set cacheLocation $pkgcache
+    choco config set cacheLocation $mainpkgcache
 }
 else {
     Write-Host "cacheLocation is already set to 'C:\ProgramData\chocolatey\cache'; moving on" -foreground Yellow
 }
 
-#The following block of bad code is to see if the package being installed is standalone or if it has dependancies
-#if a package has dependancies than the main package "ex. git" is a metapackage (a pointer) to all the other mini packages needed
-#the mini packages will come with a suffix of .install or .commandline
-#if a package has a .install then we want to focus on that package because it contains the installer files (exe or msi)
-#which are needed down the road when we need to repackage the package or SCCM/Puppet/Chef etc.
-#otherwise, if there is no .install then we need to focus on the base directory for our files
 
-#Define the packages to be install and manipulated
-choco install $pkg -y
+#install the package traditionally
+choco install $mainpkg -y
 
 
-##does the package contain a dependancy?
 #build an array of all dependencies
-[xml]$nuspec = Get-Content "$pkglib\*nuspec"
-Write-Host "We found some dependencies; $nuspec.package.metadata.dependencies.dependency"
-$pkgarray = @($pkg)
-foreach($i in ($nuspec.package.metadata.dependencies.dependency)) {
-    #do the repackaging
-    $pkgarray += $i.id
+$pkgarray = @($mainpkg)
 
+[xml]$nuspec = (Get-Content "$mainpkglib\*.nuspec")
+
+
+if ($nuspec.package.metadata.dependencies.dependency) {
+    Write-Host "We found some dependencies; standby"
+    
+    foreach($i in ($nuspec.package.metadata.dependencies.dependency)) {
+        
+        if ($i.id -ne "chocolatey-core.extension") {
+            $pkgarray += $i.id
+        }
+    }
+
+    $pkg = ($nuspec.package.metadata.dependencies.dependency).id
 }
+
+Write-Host "There are $($pkgarray.count) item(s) to package"
 $pkgarray
-Write-Host "There are $pkgarray.count items to package"
 
 
+#for each package listed in pkgarray
+#find any install files that may be stored in lib or cache
 $counter = 0 
 DO 
 {
     Write-host "Working with $pkgarray["$counter"]" -ForegroundColor Yellow
+    
     $pkg = $pkgarray["$counter"]
+    
     $pkgcache = "C:\ProgramData\chocolatey\cache"
     $pkglib = "C:\ProgramData\chocolatey\lib\$pkg"
     $pkgcache = "C:\ProgramData\chocolatey\cache\$pkg"
+    $pkgnupkg = "C:\ProgramData\chocolatey\nupkg\$pkg"
     $pkginstall = "$pkgnupkg\tools\chocolateyInstall.ps1"
+    
 
         #unzip the nupkg file
         cp "$pkglib\*.nupkg" "$pkglib\$pkg.zip"
         if(! (Test-Path $pkgnupkg)) {
+            
             New-Item -ItemType Directory -Force -Path $pkgnupkg
         }
+        
         expand-archive -path "$pkglib\$pkg.zip" -destinationpath $pkgnupkg -force
 
         #remove un-needed, soon to be recreated elements
@@ -87,125 +100,161 @@ DO
         #search for the exe files in cache
 
         #get the content of the file
-        #find only items that contain an '='
+        #find only variable assignments '$var = '
         #build a table of variables listed so that we may call upon them
-        $test = Get-Content -path $pkginstall | Where-Object {$_.Contains("=")}
-        if (($test).count -gt 0) {
-            Write-Host "Test is TRUE"
-            $chocohash = Get-Content -path $pkginstall | Where-Object {$_.Contains("=")} | ConvertFrom-StringData
+        
 
-            #select which url to use 32 vs 64
-            #parse the html for the exe name
-            #break up string by /
-            #replace the last character with nothing (its a left over ', from the 'https://BLAH'
-            #select the last parsing of /
-            #that's the file
-            if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") {
-                Write-Host "64-bit OS Architecture" -ForegroundColor "Yellow"
-                $file = ($chocohash."`$url64").split("/") -replace ".$"| Select-Object -Last 1
-            }
-            else {
-                Write-Host "32-bit OS Architecture" -ForegroundColor "Yellow"
-                $file = ($chocohash."`$url32").split("/") | Select-Object -Last 1
-            }
+        #create hashtable of chocolateyinstall.ps1 variables
+        $p = '(?<key>^\$([a-zA-Z]|\d)*[\s | \t]+\=[\s | \t]+)(?<value>.*)'
+        $chocohash = @{}
+        Select-String -Path $pkginstall -Pattern $p -AllMatches | ForEach-Object { $chocohash.add($_.Matches.Groups[2].value,$_.matches.groups[3].value) }
+      
 
-            #Lookup file in lib
-            #copy it to package directory
-            if((Get-ChildItem $pkglib -Include *.msi,*.exe,*.ignore -Recurse).count -gt 0) {
-                foreach($i in (Get-ChildItem $pkglib -Include *.msi,*.exe,*.ignore -Recurse)) {
-                    Write-Host "Copying $i from $i.fullname" -Foregroundcolor "Yellow"
-                    cp $i.fullname "$pkgnupkg\tools"
-                }
-            }
-            else {Write-Host "No files found in lib/root package directory; continuing" -ForegroundColor "Yellow"}
+        #for all the files identified in the url string, search for file in lib and cache
+        foreach($a in ($chocohash.keys | where {$_ -like "*url*" } ) ) {
+            
+            $file = ($chocohash.$a).split("/") -replace ".$" | `
+                Select-Object -Last 1
+            $file
+            Get-ChildItem -Path $pkgcache -Filter $file -Recurse | ForEach-Object { cp $_.FullName $pkgnupkg\tools}
+            Get-ChildItem -Path $pkglib -Filter $file -Recurse | ForEach-Object { cp $_.FullName $pkgnupkg\tools}
+            Get-ChildItem -Path $pkgnupkg\tools
+        }
 
-            #Lookup file in cache
-            #copy it to package directory
-            foreach ($i in (Get-ChildItem $pkgcache -Recurse | where {! $_.PSIsContainer})) {
-                Write-Host "Copying $i to $pkgnupkg\tools" -Foregroundcolor "Yellow"
-                cp $i.fullname "$pkgnupkg\tools"
-            }
-
-            #Begin to re-wrap the package for local distribution
-            <#
-            inspect chocolateyInstall.ps1
-             if web url dl, find and replace with new line
-             if exe ,  find and replace with new line
-             if zip,  find and replace with new line
-            #>
-            $webmatches = select-string -path $pkginstall -pattern "Install-ChocolateyPackage" -AllMatches
-            $exematches = select-string -path $pkginstall -pattern "Install-ChocolateyInstallPackage" -AllMatches
-            $zipmatches = select-string -path $pkginstall -pattern "Install-ChocolateyZipPackage" -AllMatches
+        #for all the files identified in the uln string, search for file in lib and cache
+        foreach($a in ($chocohash.keys | where {$_ -like "*file*" } ) ) {
+            
+            $file = ($chocohash.$a).split("\") -replace ".$" | `
+                Select-Object -Last 1
+            
+            Get-ChildItem -Path $pkgcache -Filter $file -Recurse | ForEach-Object { cp $_.FullName $pkgnupkg\tools}
+            Get-ChildItem -Path $pkglib -Filter $file -Recurse | ForEach-Object { cp $_.FullName $pkgnupkg\tools}
+            Get-ChildItem -Path $pkgnupkg\tools
+        }
 
 
-            #There are ` marks throughout these replace strings, don't mess with them unless you understand substatution
-            #The following is a REGEX stew, necessary for finding/replacing the myriad necessary flags to shape an internal package
-            #Learn your REGEX and don't fiddle with ' ` " unless you know what's up
+        #Begin to re-wrap the package for local distribution
 
-            if ($webmatches.matches.count -gt 0) {
-                #ex. notpadplusplus
-                Write-Host "Matched: $webmatches.Pattern"
+        $webmatches = select-string -path $pkginstall -pattern "Install-ChocolateyPackage" -AllMatches
+        $exematches = select-string -path $pkginstall -pattern "Install-ChocolateyInstallPackage" -AllMatches
+        $zipmatches = select-string -path $pkginstall -pattern "Install-ChocolateyZipPackage" -AllMatches
+
+
+        #There are ` marks throughout these replace strings, don't mess with them unless you understand substatution
+        #The following is a REGEX stew, necessary for finding/replacing the myriad necessary flags to shape an internal package
+        #Learn your REGEX and don't fiddle with ' ` " unless you know what's up
+
+        if ($webmatches.matches.count -gt 0) {
+            #ex. notpadplusplus
+            Write-Host "Matched: $webmatches.Pattern"
                          
-                $regexhash = @(
-                    '(\$url32[ \t]+\=\s)(.*)'
-                    '(\$url64[ \t ]+\=\s)(.*)'
-                    '(\$checksum32[ \t]+\=\s)(.*)'
-                    '(\$checksum64[ \t]+\=\s)(.*)'
-                )
+            $regexhash = @(
+                '(\$url32[ \t]+\=\s)(.*)'
+                '(\$url64[ \t ]+\=\s)(.*)'
+                '(\$checksum32[ \t]+\=\s)(.*)'
+                '(\$checksum64[ \t]+\=\s)(.*)'
+            )
                 
-                $regexcounter = 0
+            $regexcounter = 0
                 
-                DO 
-                {
-                    write-host $regexhash[$regexcounter]
+            DO 
+            {
+                write-host $regexhash[$regexcounter]
                     
                     
-                    if(($regexhash[$regexcounter]) -match "url") {
-                        $file = select-string -path $pkginstall -Pattern $regexhash[$regexcounter] | `
-                            foreach {($_.Line).split("/") -replace ".$" | `
-                            Select-Object -Last 1}
-                        $file = "$pkgnupkg/tools/$file"
-
-                    }
-                    else{
-                        $file = ''
-                    }
-                
-
-                    (Get-Content $pkginstall) | `
-                        foreach {$_ -replace $regexhash[$regexcounter],"`${1} '$file'"} | `
-                        set-content $pkginstall
-                        #out-file $pkginstall
-                    
-                    $regexcounter++
-
-                 }  while ($regexcounter -lt $regexhash.count)
-                
-            }
-
-
-            elseif ($exematches.matches.count -gt 0) {
-                #ex. 7zip
-                Write-Host "Matched: $exematches.Pattern; nothing to do"
-                
-                
-            }
-            
-            elseif ($zipmatches.Matches.Count -gt 0) {
-                #ex. notpadplusplus.commandline
-                Write-Host "Matched: $zipmatches.Pattern"
-                $find = "Install-ChocolateyZipPackage `@packageArgs"
-                $replace = "`$toolsDir   = `"`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)`"`n Install-ChocolateyZipPackage `'$pkg`' `"`$toolsdir\$file`" `"`$toolsdir`" "
-                (Get-Content $pkginstall) | foreach {$_.replace($find,$replace)} | out-file $pkginstall
-            }
-            
-            else {
-                Write-Host "Stopping: Uncertain how to handle these instructions"
-            }
-
-
+                if(($regexhash[$regexcounter]) -match "url") {
+                    $file = select-string -path $pkginstall -Pattern $regexhash[$regexcounter] | `
+                        foreach {($_.Line).split("/") -replace ".$" | `
+                        Select-Object -Last 1}
+                    $file = "$pkgnupkg/tools/$file"
 
                 }
+                else{
+                    $file = ''
+                }
+                
+
+                (Get-Content $pkginstall) | `
+                    foreach {$_ -replace $regexhash[$regexcounter],"`${1} '$file'"} | `
+                    set-content $pkginstall
+                    #out-file $pkginstall
+                    
+                $regexcounter++
+
+                }  while ($regexcounter -lt $regexhash.count)
+                
+        }
+
+
+        elseif ($exematches.matches.count -gt 0) {
+            #ex. 7zip
+            Write-Host "Matched: $exematches.Pattern; nothing to do"
+                
+                
+        }
+            
+        elseif ($zipmatches.Matches.Count -gt 0) {
+            $regexhash = @(
+                '(\$url32[ \t]+\=\s)(.*)'
+                '(\$url64[ \t ]+\=\s)(.*)'
+                '(\$checksum32[ \t]+\=\s)(.*)'
+                '(\$checksum64[ \t]+\=\s)(.*)'
+            )
+                
+            $regexcounter = 0
+                
+            DO 
+            {
+                write-host $regexhash[$regexcounter]
+                                        
+                if(($regexhash[$regexcounter]) -match "url") {
+                    
+                    $file = select-string -path $pkginstall -Pattern $regexhash[$regexcounter] | `
+                        foreach {($_.Line).split("/") -replace ".$" | `
+                        Select-Object -Last 1}
+                    
+                    $file = "$pkgnupkg/tools/$file"
+
+                }
+                else{
+                    $file = ''
+                }
+                
+                (Get-Content $pkginstall) | `
+                    foreach {$_ -replace $regexhash[$regexcounter],"`${1} '$file'"} | `
+                    set-content $pkginstall
+                    #out-file $pkginstall
+                    
+                $regexcounter++
+
+                }  while ($regexcounter -lt $regexhash.count)
+                
+        }
+
+
+        elseif ($exematches.matches.count -gt 0) {
+            #ex. 7zip
+            Write-Host "Matched: $exematches.Pattern; nothing to do"        
+                
+            #ex. notpadplusplus.commandline
+            Write-Host "Matched: $zipmatches.Pattern"
+                
+            $find = "Install-ChocolateyZipPackage `@packageArgs"
+            $replace = "`$toolsDir   = `"`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)`"`n Install-ChocolateyZipPackage `'$pkg`' `"`$toolsdir\$file`" `"`$toolsdir`" "
+                
+            (Get-Content $pkginstall) | `
+            foreach {$_.replace($find,$replace)} | `
+            out-file $pkginstall
+            
+        }
+            
+        else {
+            Write-Host "Stopping: Uncertain how to handle these instructions"
+        }
+
+
+
+                
         else {
             Write-Host "No further file searching required; this is a virtual package"
         }
